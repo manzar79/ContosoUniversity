b6d41b8adac4532aef9ef9eb94098e294b467ef9
manzar <manzar79@gmail.com>
2014-07-08 11:53:10 -0400

    add a search box
    page 62

--- ContosoUnversity/ContosoUnversity/Models/Student.cs
+++ ContosoUnversity/ContosoUnversity/Models/Student.cs
@@ -1,20 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
-        public int id { get; set; }
+        public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
         public DateTime EnrollmentDate { get; set; }
 
-        public virtual ICollection<Enrollment> Enrollments {get; set;}
-
-        
-
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
 }
