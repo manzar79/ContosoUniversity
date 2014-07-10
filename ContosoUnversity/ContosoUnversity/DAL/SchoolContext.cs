2abac6139735f5c390cd1af43f1faac1d26261a5
manzar79 <manzar79@gmail.com>
2014-07-09 21:52:05 -0400

    Async and Stored Procedures
    up to page - 222

--- ContosoUnversity/ContosoUnversity/DAL/SchoolContext.cs
+++ ContosoUnversity/ContosoUnversity/DAL/SchoolContext.cs
@@ -19,7 +19,8 @@ namespace ContosoUniversity.DAL
              .HasMany(c => c.Instructors).WithMany(i => i.Courses)             
              .Map(t => t.MapLeftKey("CourseID")                 
              .MapRightKey("InstructorID")                  
-             .ToTable("CourseInstructor"));       
+             .ToTable("CourseInstructor"));
+            modelBuilder.Entity<Department>().MapToStoredProcedures();
         }
     }
 }
